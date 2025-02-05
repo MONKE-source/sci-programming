c1ef2e0afc4b24344746212009d6d6f2f0263dc7
Jonathan Soh <jonathan@Jonathans-MacBook-Air-2.local>
2025-02-05 09:37:17 +0800

    jonathons files

---
+++ bisection/jonathan.py
@@ -0,0 +1,14 @@
+import math
+def bisection(xl,xr,sf):
+    while (xr-xl) > (10**-sf):
+        xm = (xl+xr)/2
+        if math.tan(xm) - xm > 0:
+            xr = xm
+        elif math.tan(xm) - xm < 0:
+            xl = xm
+        print(xr,xl)
+        print((xr-xl),(10**-sf))
+    return round((xr+xl)/2,sf)
+
+bisection(4,5,15)
+            
