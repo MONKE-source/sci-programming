4f4faedde7b846303f578b7754a74029fa6aa238
Jonathan Soh <jonathan@Jonathans-MacBook-Air-2.local>
2025-02-05 10:25:47 +0800

    accurate sf

--- bisection/jonathan.py
+++ bisection/jonathan.py
@@ -1,14 +1,17 @@
 import math
+from decimal import Decimal, getcontext
 def bisection(xl,xr,sf):
+    getcontext().prec = sf+20
+    xl = Decimal(xl)
+    xr = Decimal(xr)
     while (xr-xl) > (10**-sf):
-        xm = (xl+xr)/2
-        if math.tan(xm) - xm > 0:
+        xm = Decimal((xl+xr)/2)
+        if Decimal(math.tan(xm)) - xm > 0:
             xr = xm
-        elif math.tan(xm) - xm < 0:
+        elif Decimal(math.tan(xm)) - xm < 0:
             xl = xm
-        print(xr,xl)
-        print((xr-xl),(10**-sf))
     return round((xr+xl)/2,sf)
 
-bisection(4,5,15)
-print("hello world")
+f = bisection(4,5,50)
+print(f)    
+
