54ea5d4697820eed4f962d136a62b8affbf45182
Arth Aggarwal <130353466+arthaggarwal@users.noreply.github.com>
2025-03-27 09:22:54 +0800

    Update arth.py

--- bisection/arth.py
+++ bisection/arth.py
@@ -1,24 +1,20 @@
 import math
 from decimal import Decimal, getcontext
 
-# decimal makes it so that there will still be high precision and wtv number of decimal places
 
 
 getcontext().prec = (
-    22  # sets the decimal to 22 dp, extra 2 for some more added precision
-)
+    22  
+) # sets the decimal to 22 dp
 
 
-# defines the function that will be going through the bisection
 def function(x):
     return (
         Decimal(math.tan(float(x))) - x
-    )  # have to put float and decimal if not no run idk
+    )  
 
 
-a, b = Decimal(1), Decimal(
-    3
-)  # these are the initial endpoints, using the decimal function so that it will be to 22 dp
+a, b = Decimal(1), Decimal(3)  
 tolerance = 0.00000000000000000001  # defines how small the root should be
 
 
